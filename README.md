3bc00b561772f0193642b1897bef672c70dcb13d
briangonzales1996 <brianjose00@gmail.com>
2025-02-04 22:52:58 -0300

    xx

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+<em>Challenge amigo secreto</em>
+
 <h1>Challenge amigo secreto</h1>
 <p>Aqui les dejo mi pequeño proyecto JS para challenge</p>
+
+<h2>Introdución</h2>
+   
--- js/op.js
+++ js/op.js
@@ -1,78 +1,88 @@
 const usuarios = [];
 
-const limpiarHTML = ()=>{
+const limpiarHTML = () => {
     const winnerConatiner = document.getElementById('cartel-winner')
-    winnerConatiner.innerHTML='';
+    winnerConatiner.innerHTML = '';
     const contenedor = document.getElementById('usuarios');
-    contenedor.innerHTML ='';
+    contenedor.innerHTML = '';
     return contenedor;
 }
 
-const limpiarInput = (advertencia)=>{
+const limpiarInput = (advertencia) => {
     const valueName = document.getElementById("valueNombre");
     valueName.value = '';
-    if(advertencia)alert(advertencia);
+    if (advertencia) alert(advertencia);
 }
 
-const addNames = (name)=>{
+const addNames = (name) => {
     let fragment = document.createDocumentFragment();
     usuarios.push(name);
     const contenedor = limpiarHTML();
-    usuarios.forEach((usuario)=>{
-        fragment = agregarHTML(fragment,usuario);
+    usuarios.forEach((usuario) => {
+        fragment = agregarHTML(fragment, usuario);
     })
-    contenedor.appendChild (fragment);
+    contenedor.appendChild(fragment);
 }
 
 
 
-const agregarHTML = (fragment='',name='',texto='') =>{
+const agregarHTML = (fragment = '', name = '', texto = '') => {
     const parrafo = document.createElement('P');
-    parrafo.textContent =  texto + name ;
+    parrafo.textContent = texto + name;
     fragment.appendChild(parrafo);
     return fragment;
 }
 
 //add cartel of winner
-const agregarHtmlWinner = (usuarios,winner) =>{
+const agregarHtmlWinner = (usuarios, winner) => {
     let fragment = document.createDocumentFragment();
-   // alert('el ganador del sorteo es '+ usuarios[winner]);
+    // alert('el ganador del sorteo es '+ usuarios[winner]);
     const winnerConatiner = document.getElementById('cartel-winner');
-    winnerConatiner.innerHTML='';
-    fragment= agregarHTML(fragment,usuarios[winner],'El ganador del sorteo es: ');
+    winnerConatiner.innerHTML = '';
+    fragment = agregarHTML(fragment, usuarios[winner], 'El ganador del sorteo es: ');
     winnerConatiner.appendChild(fragment);
-    
+
 }
 
-const selectUser = ()=>{
-    const winner = Math.floor(Math.random()*usuarios.length)
-    agregarHtmlWinner(usuarios,winner)
+const selectUser = () => {
+    const winner = Math.floor(Math.random() * usuarios.length)
+    agregarHtmlWinner(usuarios, winner)
 }
 
 
 //boton de sortear usuario
-const sortearUser = (e)=>{
-    usuarios.length>=5?selectUser():alert("debe por lo menos 5 participantes")
+const sortearUser = (e) => {
+    usuarios.length >= 5 ? selectUser() : alert("debe por lo menos 5 participantes")
 }
 
+const verificarLetras = (verificar) => {
+    let comprobar = true;
+    for (let i = 0; i < verificar.length; i++) {
+        if (!isNaN(parseInt(verificar[i]))){
+            return false
+        }
+    }
+    return comprobar;
+}
 
 //evaluar el nombre valido
-const agregarParticipantes = (verificar)=>{
+const agregarParticipantes = (verificar) => {
     const input = document.querySelector('#valueNombre');
-    console.log("esto es name ",verificar);
-    if(verificar.length>1 && isNaN(verificar)){
+
+    if (verificar.length > 1 && verificarLetras(verificar) && isNaN(verificar)) {
         addNames(verificar);
         input.value = '';
+
     }
     else {
         limpiarInput();
-        alert("ingrese un nombre valido" );
+        alert("ingrese un nombre valido y que no contenga numeros");
     }
 }
 
-const verificarNameRepeat = (verificar,condicion)=>{
-    for(let i=0;i<usuarios.length;i++){
-        if(usuarios[i]===verificar)condicion=true;
+const verificarNameRepeat = (verificar, condicion) => {
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i] === verificar) condicion = true;
     }
     return condicion
 }
@@ -86,10 +96,10 @@ function handleClick(e) {
     const name = document.querySelector('#valueNombre');
     const verificar = name.value.toLowerCase();
     let condicion = false;
-    condicion = verificarNameRepeat(verificar,condicion);
-    condicion?
-    limpiarInput('El nombre ya se encuentra registrado')
-    :agregarParticipantes(verificar);
+    condicion = verificarNameRepeat(verificar, condicion);
+    condicion ?
+        limpiarInput('El nombre ya se encuentra registrado')
+        : agregarParticipantes(verificar);
 }
 
 
@@ -97,5 +107,5 @@ function handleClick(e) {
 //INCIO DEL PROGRAMA
 const add = document.querySelector("#agregar");
 const sortear = document.querySelector('#sortear');
-add.addEventListener('click',handleClick);
-sortear.addEventListener('click',sortearUser);
+add.addEventListener('click', handleClick);
+sortear.addEventListener('click', sortearUser);
